21d639b3c6bda9182b9556c6ec863688c26e2549
Chloe Roux <chloe.roux.lorieux@gmail.com>
2022-02-15 16:25:00 +0100

    not styled yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Creating 100 fake articles...'
+100.times do
+  article = Article.new(
+    title: Faker::BossaNova.song,
+    content: Faker::TvShows::BigBangTheory.quote,
+  )
+  article.save!
+end
+puts 'Finished!'
